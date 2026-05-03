a8991e38bdbdae20fbee0a5992fc2699b7eb1e11
otnick <116823292+otnick@users.noreply.github.com>
2026-05-03 18:53:41 +0200

    debug

--- Assets/Scripts/Core/RoleManager.cs
+++ Assets/Scripts/Core/RoleManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+
 // Manages the local player's role and provides methods to change it
 public class RoleManager : MonoBehaviour
 {
@@ -8,18 +10,30 @@ public class RoleManager : MonoBehaviour
 
     public PlayerRole LocalPlayerRole => localPlayerRole;
 
+    public event Action<PlayerRole> RoleChanged;
+
     private void Awake()
     {
         Instance = this;
     }
 
+    public void SetRole(PlayerRole role)
+    {
+        if (localPlayerRole == role) return;
+
+        localPlayerRole = role;
+        RoleChanged?.Invoke(localPlayerRole);
+
+        Debug.Log($"Role changed to: {localPlayerRole}");
+    }
+
     public void SetLocalRoleKitchen()
     {
-        localPlayerRole = PlayerRole.Kitchen;
+        SetRole(PlayerRole.Kitchen);
     }
 
     public void SetLocalRoleFront()
     {
-        localPlayerRole = PlayerRole.Front;
+        SetRole(PlayerRole.Front);
     }
 }
