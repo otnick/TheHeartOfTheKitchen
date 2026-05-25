b1d3aefbb53b3efeeb5b05c0477ee374ce7c21f3
Kipras Klimkevičius <kipras.klim@gmail.com>
2026-05-25 10:45:50 +0200

    add scene for tangible and add one food per session to grinder

--- Assets/Scripts/MeatGrinder.cs
+++ Assets/Scripts/MeatGrinder.cs
@@ -7,6 +7,7 @@ public class MeatGrinder : MonoBehaviour
     [Header("Food Item Prefab")]
     [SerializeField]
     GameObject foodPrefab;
+    bool spawnedFood = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is 
 
     // Update is called once per frame
@@ -17,8 +18,11 @@ public class MeatGrinder : MonoBehaviour
 
     public void SpawnMeatGround()
     {
+        if (spawnedFood) return;
         var runner = FindFirstObjectByType<NetworkRunner>();
         if (runner == null) return;
-        runner.Spawn(foodPrefab, transform.position, transform.rotation);
+        NetworkObject meat = runner.Spawn(foodPrefab, transform.position, transform.rotation);
+        meat.GetComponent<Rigidbody>().AddForce(Vector3.up * 10, ForceMode.Impulse);
+        spawnedFood=true;
     }
 }
