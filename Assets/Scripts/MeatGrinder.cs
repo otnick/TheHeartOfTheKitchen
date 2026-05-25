83c16fe1c6236144c800bee87fa1390acb7a5d2a
Kipras Klimkevičius <kipras.klim@gmail.com>
2026-05-25 11:26:26 +0200

    add signifiers to food grinder

--- Assets/Scripts/MeatGrinder.cs
+++ Assets/Scripts/MeatGrinder.cs
@@ -1,13 +1,20 @@
 using Fusion;
 using Meta.XR.MRUtilityKit.SceneDecorator;
+using System.Collections;
 using UnityEngine;
+using static Unity.Collections.Unicode;
 
 public class MeatGrinder : MonoBehaviour
 {
     [Header("Food Item Prefab")]
     [SerializeField]
     GameObject foodPrefab;
+    [Header("Spawn Smoke Prefab")]
+    [SerializeField]
+    GameObject spawnSmokeEffect;
     bool spawnedFood = false;
+    [SerializeField]
+    AudioSource foodAudioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is 
 
     // Update is called once per frame
@@ -20,9 +27,25 @@ public class MeatGrinder : MonoBehaviour
     {
         if (spawnedFood) return;
         var runner = FindFirstObjectByType<NetworkRunner>();
-        if (runner == null) return;
+        if (runner == null)
+        {
+            Debug.Log("NoRunner");
+            return;
+        }
+        spawnedFood=true;
+        foodAudioSource.Play();
+        Debug.Log("Playing spawning sound");
+        IEnumerator coroutine = SpawnFood(runner);
+        StartCoroutine(coroutine);
+
+    }
+    IEnumerator SpawnFood(NetworkRunner runner)
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("we did that spawning thing, remember?");
         NetworkObject meat = runner.Spawn(foodPrefab, transform.position, transform.rotation);
         meat.GetComponent<Rigidbody>().AddForce(Vector3.up * 10, ForceMode.Impulse);
-        spawnedFood=true;
+        foodAudioSource.Stop();
+        Instantiate(spawnSmokeEffect, transform.position, transform.rotation);
     }
 }
